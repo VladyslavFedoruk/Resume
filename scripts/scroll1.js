33ec036ccd68f978a91b857f646751d30b9140b5
“Vladyslav.Fedoruk” <mr.fedoruk17@gmail.com>
2023-04-25 03:11:34 +0300

    second

--- scripts/scroll1.js
+++ scripts/scroll1.js
@@ -1,5 +1,26 @@
-let button = document.querySelector('.item1');
-let sectionTwo = document.querySelector('.hero_content-descr');
+let button0 = document.querySelector('.item1');
+let sectionFirst = document.querySelector('.hero');
+
+function scrollTo(element) {
+    window.scroll({
+        left: 0,
+        top: element.offsetTop,
+        behavior: 'smooth',
+    })
+}
+
+if (button0 && sectionFirst) {
+    button0.addEventListener('click', () => {
+        scrollTo(sectionFirst);
+    })
+}
+
+
+
+
+
+let button = document.querySelector('.item2');
+let sectionTwo = document.querySelector('.skills');
 
 function scrollTo(element) {
     window.scroll({
@@ -12,5 +33,22 @@ function scrollTo(element) {
 if (button && sectionTwo) {
     button.addEventListener('click', () => {
         scrollTo(sectionTwo);
+    })
+}
+
+let button1 = document.querySelector('.item3');
+let sectionThree = document.querySelector('.qualification');
+
+function scrollTo(element) {
+    window.scroll({
+        left: 0,
+        top: element.offsetTop,
+        behavior: 'smooth',
+    })
+}
+
+if (button1 && sectionThree) {
+    button1.addEventListener('click', () => {
+        scrollTo(sectionThree);
     })
 }
